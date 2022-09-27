554572bdc6ea12f2936867bd9733161dca08ca32
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-09-27 21:22:37 +0200

    Added support for querying contributions from the UI

--- fp-website/src/main/html/common/js/add-repo.js
+++ fp-website/src/main/html/common/js/add-repo.js
@@ -36,9 +36,64 @@ function updateStates(transactionId) {
             .catch(function (error) {
                 console.log(error)
             });
-
 }
 
+
+function loadImages() {
+    axios.get("http://localhost:8080/api/info", {})
+        .then(function (response) {
+            console.log(response)
+            $("#project-version").text(response.data.projectVersion)
+            $("#camel-version").text(response.data.camelVersion)
+            $("#kafka-client-version").text(response.data.kafkaClientVersion)
+        })
+        .catch(function (error) {
+            console.log(error)
+
+            $("#project-version").text("undefined")
+            $("#camel-version").text("undefined")
+            $("#kafka-client-version").text("undefined")
+        });
+}
+
+function searchContributions() {
+    var contribEmail = document.getElementById("contrib");
+    var searchContribApi = "http://localhost:8080/api/" + $('[search-form]').attr('api') + contribEmail.value
+
+    console.debug("Using API: " + searchContribApi);
+    console.debug("Search for a contribution using email: " + contribEmail.value);
+
+//    $("#wait-for-repo-spinner").show();
+//    $("#search-form").fadeOut();
+
+     axios
+            .get(searchContribApi)
+            .then(function(response) {
+//                $("#wait-for-repo-spinner").hide();
+//                $("#search-form").hide();
+                $("#contrib-qrs").show();
+
+                console.debug(response)
+                console.debug("Done searching for contribution: " + response.data.state)
+
+                for (let i = 0; i < response.data.contributionList.length; i++) {
+                    console.log(response.data.contributionList[i].commitInfo.message)
+
+                    var div = "<p>" + response.data.contributionList[i].commitInfo.message + "</p>"
+
+                    var img = "<img uk-cover height=\"256\" width=\"256\" src=\"data:image/png;base64," + response.data.contributionList[i].encodedQrCode + "\">"
+
+                    $("#slideshow-qrs").append("<li>" + img + div + "</li>")
+                }
+            })
+            .catch(function(error) {
+                console.log(error)
+            })
+            console.log("Done searching repo")
+        // We do not want to reload the page
+        return false;
+
+}
 
 function addRepository() {
     var newRepoApi = "http://localhost:8080/api/" + $('[new-repo-form]').attr('api');
@@ -66,7 +121,9 @@ function addRepository() {
 
                     if (completed == true || count > 60) {
                         clearInterval(timeout)
-                        $("#repo-form").show();
+                        $("#repo-form").hide();
+//                        $("#repo-form").show();
+                        $("#search-form").show();
                         $("#wait-for-repo-spinner").hide();
 
                         lastState = ""
@@ -87,6 +144,8 @@ $(document).ready(function () {
     console.debug("Loading page")
 
     $("#wait-for-repo-spinner").hide();
+    $("#search-form").hide();
+    $("#contrib-qrs").hide();
 
     axios.get("http://localhost:8080/api/info", {})
         .then(function (response) {
